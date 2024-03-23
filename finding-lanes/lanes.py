c3c23e0b2f68528f91859368e0ac5d31dac1092a
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-23 20:52:58 +0000

    implementing hough transform(ation)

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -8,15 +8,27 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (160, 32, 240), 10)
+    return line_image
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
-cv2.imshow('result', region_of_interest(canny))
+cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+cv2.imshow('result', line_image)
 cv2.waitKey(0)
