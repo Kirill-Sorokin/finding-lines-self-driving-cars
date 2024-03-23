56a2931bcc14a691aa351f0dc4d6156a5c63dca7
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-23 23:05:37 +0000

    idenity lines within a gradient image

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -21,7 +21,7 @@ def region_of_interest(image):
     polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    masked_image = cv2.bitwise_and(image, mask)
+    masked_image = cv2.bitwise_and(canny, mask)
     return masked_image
 
 image = cv2.imread('test_image.jpg')
@@ -30,5 +30,6 @@ canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
 line_image = display_lines(lane_image, lines)
-cv2.imshow('result', line_image)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow("result", combo_image)
 cv2.waitKey(0)
